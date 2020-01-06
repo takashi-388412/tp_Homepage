3001aec560ec6bb067860151b9da1c689defe5be
takashi-388412 <dena388412@gmail.com>
2020-01-06 13:41:32 +0900

    no message

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create()
+
+gulp.task('serve', function (done) {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'index.html',
+    },
+  })
+  done();
+})
+gulp.task('bs-reload', function (done) {
+  browserSync.reload();
+  done();
+})
+gulp.task('watch', function (done) {
+  gulp.watch('./*.html', gulp.task('bs-reload'));
+  gulp.watch('./css/*.css', gulp.task('bs-reload'));
+})
+
+gulp.task('default', gulp.series('serve', 'watch'))
